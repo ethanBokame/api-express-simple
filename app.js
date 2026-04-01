e319d5665c4a42af61e307340c4a48c1e5e62b9c
ethanbokame <ethaniraqui@gmail.com>
2026-04-01 08:16:26 +0000

    ajout du port en log

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ const UserRoutes = require("./routes/UserRoutes");
 app.use(express.json());
 app.use("/api/users", UserRoutes);
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
