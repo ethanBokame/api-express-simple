01bd31381618c8a398bc327908f4fd876b4dc1cc
ethanbokame <ethaniraqui@gmail.com>
2026-04-01 08:54:18 +0000

    autorisation cors + comments

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express()
 const UserRoutes = require("./routes/UserRoutes");
 
 // middlewares
-app.use(express.json());
-app.use("/api/users", UserRoutes);
+app.use(express.cors()); // Enable CORS for all routes
+app.use(express.json()); // Parse JSON bodies for all routes
+app.use("/api/users", UserRoutes); // Use the user routes for /api/users endpoint
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
