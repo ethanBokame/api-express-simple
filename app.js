5e7dc74f74d788a57494c005bd7de7ad2e1ec218
ethanbokame <ethaniraqui@gmail.com>
2026-04-01 09:13:45 +0000

    import correct des cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+const port = 3000
 const UserRoutes = require("./routes/UserRoutes");
 
 // middlewares
-app.use(express.cors()); // Enable CORS for all routes
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Parse JSON bodies for all routes
 app.use("/api/users", UserRoutes); // Use the user routes for /api/users endpoint
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(port, () => { 
+    console.log(`Server is running on port ${port}`);
 })
